f38a463a4fe80bafccc33c25fee37caab3a54c2e
Igor Silva Alves <igorsaj@hotmail.com>
2022-07-26 14:16:30 -0300

    feat: add EntityNotFoundExceptionFilter

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { EntityNotFoundExceptionFilter } from './exception-filters/entity-not-found.exception-filter';
 
 const port = process.env.PORT || 3000;
 const logger = new Logger();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new EntityNotFoundExceptionFilter());
+
   await app.listen(port, () => {
     logger.log(`Server listening on port ${port}`, 'Bootstrap');
   });
