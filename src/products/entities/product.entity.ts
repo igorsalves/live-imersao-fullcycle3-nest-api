db9483d4be42bae0a273254c9b1a04e6f2aa7bff
Igor Silva Alves <igorsaj@hotmail.com>
2022-07-26 14:18:33 -0300

    feat: definido campos

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,13 @@
-export class Product {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'products' })
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  price: number;
+}
