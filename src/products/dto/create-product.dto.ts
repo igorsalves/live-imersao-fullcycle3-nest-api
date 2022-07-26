c825d787a8fe881fad359a58f7f39962c2803ce4
Igor Silva Alves <igorsaj@hotmail.com>
2022-07-26 14:18:53 -0300

    feat: class-validator

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,12 @@
-export class CreateProductDto {}
+import { IsNotEmpty, IsNumber, IsString, MaxLength } from 'class-validator';
+
+export class CreateProductDto {
+  @MaxLength(255)
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  price: number;
+}
